d4ffe6152f628e4526934adfd7d735f4d3be7cf1
Alex Harkess <aharkess@plantbio.uga.edu>
2014-09-06 02:13:34 -0400

    added: fetch TOS .csv files from url

--- R/tosr-package.r
+++ R/tosr-package.r
@@ -3,3 +3,14 @@
 #' @name tosr
 #' @docType package
 NULL
+
+# pull a local copy of the database into memory
+# working with plants for now
+
+# future -- add @opts for which db to download
+
+plant_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-3/files/plantsTreeOfSex.csv"))
+vertebrate_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads/files/vertebrateTreeOfSex.csv"))
+invertebrate_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-2/files/invert.data.csv"))
+
+
