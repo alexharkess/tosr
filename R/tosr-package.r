b9c5227729903a62a37e8bc4fe274be7c4c3bb54
Alex Harkess <aharkess@plantbio.uga.edu>
2014-09-07 11:47:33 -0400

    added test newick; db downloads now working

--- R/tosr-package.r
+++ R/tosr-package.r
@@ -5,21 +5,27 @@
 NULL
 
 # pull a local copy of the database into memory
-# ex:  extract_db("plant")
+# ex:  extract_db("plant") or extract_db("plant","vertebrate")
 
 extract_db <- function(...) {
-	opts <- list(...)
+	# making opts a global variable to check in the future if users dl'ed more than one database
+	opts <<- list(...)
 	if ('plant' %in% opts) {
-		cat("loading plant db")
-		return(plant_tos<-read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-3/files/plantsTreeOfSex.csv")))
+		cat("loading plant db\n")
+		plant_tos <<- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-3/files/plantsTreeOfSex.csv"))
 	}
 	if ('vertebrate' %in% opts) {
-		cat("loading vertebrate db")
-		return(vertebrate_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads/files/vertebrateTreeOfSex.csv")))
+		cat("loading vertebrate db\n")
+		vertebrate_tos <<- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads/files/vertebrateTreeOfSex.csv"))
 	}
 	if ('invertebrate' %in% opts) {
-		cat("loading invertebrate db")
-		return(invertebrate_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-2/files/invert.data.csv")))
+		cat("loading invertebrate db\n")
+		invertebrate_tos <<- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-2/files/invert.data.csv"))
 		}
 }
+
+# pull in a users newick tree with ape
+# prerequisite issues : users need to be sure their newick species names are identical to the ToS names
+# test tree in tests/asparagus.tos.newick
+# ex: myTree <- read.newick("")
 
