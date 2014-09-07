5810111e0110c92594dfe229bc7aee4f9db176e8
Alex Harkess <aharkess@plantbio.uga.edu>
2014-09-06 20:25:08 -0400

    wrote function extract_db() to download local database copy

--- R/tosr-package.r
+++ R/tosr-package.r
@@ -5,12 +5,21 @@
 NULL
 
 # pull a local copy of the database into memory
-# working with plants for now
+# ex:  extract_db("plant")
 
-# future -- add @opts for which db to download
-
-plant_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-3/files/plantsTreeOfSex.csv"))
-vertebrate_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads/files/vertebrateTreeOfSex.csv"))
-invertebrate_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-2/files/invert.data.csv"))
-
+extract_db <- function(...) {
+	opts <- list(...)
+	if ('plant' %in% opts) {
+		cat("loading plant db")
+		return(plant_tos<-read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-3/files/plantsTreeOfSex.csv")))
+	}
+	if ('vertebrate' %in% opts) {
+		cat("loading vertebrate db")
+		return(vertebrate_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads/files/vertebrateTreeOfSex.csv")))
+	}
+	if ('invertebrate' %in% opts) {
+		cat("loading invertebrate db")
+		return(invertebrate_tos <- read.csv(url("http://bbrowse.biol.berkeley.edu/treeV2/styled/downloads-2/files/invert.data.csv")))
+		}
+}
 
